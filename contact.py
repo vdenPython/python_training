edf9545edc9101a768a758a957e87108714da2b8
Denisov Vasiliy <vden@3dhm.ru>
2015-08-02 18:07:32 +0600

    Задание №3 Вспомагательные методы и классы для контактов

---
+++ contact.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+__author__ = 'vden'
+
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nickname, title, company,
+                 address, home, mobile, work, fax, address2, phone2, notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home = home
+        self.mobile = mobile
+        self.work = work
+        self.fax = fax
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
