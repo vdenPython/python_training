4214be4ee6f5797de816d1442325fd57096957f4
Denisov Vasiliy <vden@3dhm.ru>
2015-08-07 15:26:48 +0600

    Созданы пакеты для разных зон ответственности

--- test/test_add_data.py
+++ test/test_add_data.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import pytest
-from group import Group
-from contact import Contact
-from application import Application
+from model.group import Group
+from model.contact import Contact
+from fixture.application import Application
 
 
 @pytest.fixture
