c82998403600c4be757dbda62feda076241d896a
Denisov Vasiliy <vden@3dhm.ru>
2015-08-17 21:12:16 +0600

    test modify group

--- fixture/group.py
+++ fixture/group.py
@@ -15,24 +15,41 @@ class GroupHelper:
         self.app.navigation.open_page(name_page="groups")
         # init group creation
         wd.find_element_by_name("new").click()
-        # fill group firm
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group_form(group)
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
 
+    def fill_group_form(self, group):
+        wd = self.app.wd
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+
     def delete_first_group(self):
         wd = self.app.wd
         self.app.navigation.open_page(name_page="groups")
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group()
         wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+
+    def select_first_group(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+
+    def modify_first_group(self, new_group_date):
+        wd = self.app.wd
+        self.app.navigation.open_page(name_page="groups")
+        self.select_first_group()
+        wd.find_element_by_name("edit").click()
+        self.fill_group_form(new_group_date)
+        wd.find_element_by_name("update").click()
         self.return_to_groups_page()
 
--- model/group.py
+++ model/group.py
@@ -4,7 +4,7 @@ __author__ = 'vden'
 
 class Group:
 
-    def __init__(self, name, header, footer):
+    def __init__(self, name=None, header=None, footer=None):
         self.name = name
         self.header = header
         self.footer = footer
