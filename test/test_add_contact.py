9374e0b773f5e74501de3e8c63f13d8f9a7872dd
Denisov Vasiliy <vden@3dhm.ru>
2015-09-20 19:33:54 +0600

    Задание №19: Реализована загрузка тестовых данных из файлов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,37 +1,10 @@
 # -*- coding: utf-8 -*-
 __author__ = 'vden'
 from model.contact import Contact
-import pytest
-import random
-import string
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters+string.digits+" "*10
-    return prefix+"".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Contact(firstname="", middlename="", lastname="", nickname="", title="", company="",
-                    address="", homephone="", mobilephone="", workphone="", fax="", address2="",
-                    seconderyphone="", notes="")]+ [Contact(firstname=random_string('firstname', 10),
-                                                    middlename=random_string('middlename', 10),
-                                                    lastname=random_string('lastname', 10),
-                                                    nickname=random_string('nickname', 10),
-                                                    title=random_string('title', 10),
-                                                    company=random_string('company', 10),
-                                                    address=random_string('address', 10),
-                                                    homephone=random_string('home', 10),
-                                                    mobilephone=random_string('mobile', 10),
-                                                    workphone=random_string('work', 10),
-                                                    fax=random_string('fax', 10),
-                                                    address2=random_string('address2', 10),
-                                                    seconderyphone=random_string('phone2', 10),
-                                                    notes=random_string('notes', 10)) for i in range(5)
-]
-
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     assert len(old_contacts)+1 == app.contact.count()
