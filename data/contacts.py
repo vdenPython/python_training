3df531e696f756ecba44cb0c39b207000a0b6169
Denisov Vasiliy <vden@3dhm.ru>
2015-09-20 19:19:22 +0600

    Задание №18: Реализованы генераторы тестовых данных

---
+++ data/contacts.py
@@ -0,0 +1,12 @@
+from model.contact import Contact
+
+testdata = [
+    Contact(firstname="firstname1", middlename="middlename1", lastname="lastname1", nickname="nickname1",
+            title="title1", company="company1", address="address1", homephone="homephone1",
+            mobilephone="mobilephone1", workphone="workphone1", fax="fax1",
+            address2="address21", seconderyphone="seconderyphone1", notes="notes1"),
+    Contact(firstname="firstname2", middlename="middlename2", lastname="lastname2", nickname="nickname2",
+            title="title2", company="company2", address="address2", homephone="homephone2",
+            mobilephone="mobilephone2", workphone="workphone2", fax="fax2",
+            address2="address22", seconderyphone="seconderyphone2", notes="notes2")
+]
