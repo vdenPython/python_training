1bf16479cca43cae8a1089001511e94b377f0f1a
Denisov Vasiliy <vden@3dhm.ru>
2015-09-23 20:09:10 +0600

    7_3 video

--- conftest.py
+++ conftest.py
@@ -24,7 +24,7 @@ def load_config(file):
 def app(request):
     global fixture
     browser = request.config.getoption("--browser")
-    web_config = load_config(request.config.getoption("--target"))("web")
+    web_config = load_config(request.config.getoption("--target"))["web"]
     if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser, base_url=web_config["baseUrl"])
     fixture.session.ensure_login(username=web_config["username"], password=web_config["password"])
@@ -33,7 +33,7 @@ def app(request):
 
 @pytest.fixture(scope='session')
 def db(request):
-    db_config = load_config(request.config.getoption("--target"))("db")
+    db_config = load_config(request.config.getoption("--target"))["db"]
     dbfixture = Dbfixture(host=db_config["host"], name=db_config["name"], user=db_config["user"], password=db_config["password"])
     def fin():
         dbfixture.destroy()
